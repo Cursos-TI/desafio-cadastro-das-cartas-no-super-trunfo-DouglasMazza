89f6f0811f9e22dc3a869bd609e898edb297a46e
DouglasMazza <mazza.douglas13@gmail.com>
2025-08-09 17:24:20 +0000

    CartasSuperTrunfo Final

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 int main() {
+    
+    char estado1[10], estado2[10];
     char codigo1[10], codigo2[10];
+    char cidade1[50], cidade2[50]; 
     int populacao1, populacao2;
     float area1, area2;
     float pib1, pib2;
@@ -10,8 +13,12 @@ int main() {
     // Iniciando a programação
     
     printf("Cadastro da Carta 1:\n"); // Obtendo informação da primeira carta
+    printf("Informe o estado (A,B,C,D,E,F,G ou H):");
+    scanf("%s", estado1);
     printf("Digite o código (ex: A01): ");
     scanf("%s", codigo1);
+    printf("Digite o nome da cidade:");
+    scanf("%s", cidade1);
     printf("Digite a população: ");
     scanf("%d", &populacao1);
     printf("Digite a área (km²): ");
@@ -22,8 +29,12 @@ int main() {
     scanf("%d", &pontos1);
 
     printf("\nCadastro da Carta 2:\n"); // Obtendo informação da segunda carta
+     printf("Informe o estado (A,B,C,D,E,F,G ou H):");
+    scanf("%s", estado2);
     printf("Digite o código (ex: B02): ");
     scanf("%s", codigo2);
+    printf("Digite o nome da cidade:");
+    scanf("%s", cidade2);
     printf("Digite a população: ");
     scanf("%d", &populacao2);
     printf("Digite a área (km²): ");
@@ -36,14 +47,18 @@ int main() {
     // Resultado
 
     printf("\n--- Carta 1 ---\n");
+    printf("Estado: %s\n", estado1);
     printf("Código: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", cidade1);
     printf("População: %d\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Pontos turísticos: %d\n", pontos1);
 
     printf("\n--- Carta 2 ---\n");
+    printf("Estado: %s\n", estado2);
     printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", cidade2);
     printf("População: %d\n", populacao2);
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
